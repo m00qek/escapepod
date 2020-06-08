bb6c995b45d073ccd833d58b53867b15ed631b1d
António Mora <m00qek@criptext.com>
2020-06-08 14:09:18 -0300

    Change name and use http

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from fileinput import FileInput
 import xml.etree.ElementTree as ET
 import glob
 
@@ -16,3 +17,7 @@ if __name__ == "__main__":
     load_episodes(channel, 'episodes/rss-2')
 
     feed.write('feeds/generated/all.xml')
+
+    with FileInput('feeds/generated/all.xml', inplace=True) as file:
+        for line in file:
+            print(line.replace('http://', 'https://'), end='')
